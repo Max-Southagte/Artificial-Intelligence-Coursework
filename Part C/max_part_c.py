c4a57fac3c16056dd06cd49d6c26bb55ce969a5e
Max Southgate <max1southgate@gmail.com>
2024-12-05 11:20:40 +0000

    changed varibale names for example and part C and made it use the sigmoid functions instead of tanh

--- Part C/max_part_c.py
+++ Part C/max_part_c.py
@@ -2,65 +2,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(42)
-x = np.linspace(-10, 10, 100)
-y_true = 3 * x + 0.7 * x**2
+x_values = np.linspace(-10, 10, 100)
+y_actual = 3 * x_values + 0.7 * x_values**2
 
 class SimpleNeuralNetwork:
-    def __init__(self, input_dim, hidden_dim, output_dim, learning_rate=0.01):
-        self.w1 = np.random.randn(input_dim, hidden_dim) * 0.1
-        self.b1 = np.zeros(hidden_dim)
-        self.w2 = np.random.randn(hidden_dim, output_dim) * 0.1
-        self.b2 = np.zeros(output_dim)
+    def __init__(self, input_size, hidden_size, output_size, learning_rate=0.01):
+        self.weights_input_hidden = np.random.randn(input_size, hidden_size) * 0.1
+        self.bias_hidden = np.zeros(hidden_size)
+        self.weights_hidden_output = np.random.randn(hidden_size, output_size) * 0.1
+        self.bias_output = np.zeros(output_size)
         self.learning_rate = learning_rate
 
-    def forward(self, x):
-        self.z1 = np.dot(x, self.w1) + self.b1
-        self.a1 = np.tanh(self.z1)
-        self.z2 = np.dot(self.a1, self.w2) + self.b2
-        return self.z2
+    def sigmoid(self, x):
+        return 1.0 / (1.0 + np.exp(-x))
+    
+    def sigmoid_derivative(self, x):
+        return x * (1.0 - x)
 
-    def backward(self, x, y, y_pred):
-        error = y_pred - y
-        d_w2 = np.dot(self.a1.T, error) / len(x)
-        d_b2 = np.mean(error, axis=0)
-        d_a1 = np.dot(error, self.w2.T) * (1 - np.tanh(self.z1)**2)
-        d_w1 = np.dot(x.T, d_a1) / len(x)
-        d_b1 = np.mean(d_a1, axis=0)
+    def forward(self, inputs):
+        self.hidden_layer_input = np.dot(inputs, self.weights_input_hidden) + self.bias_hidden
+        self.hidden_layer_output = self.sigmoid(self.hidden_layer_input)
+        self.output_layer_input = np.dot(self.hidden_layer_output, self.weights_hidden_output) + self.bias_output
+        return self.output_layer_input
 
-        self.w2 -= self.learning_rate * d_w2
-        self.b2 -= self.learning_rate * d_b2
-        self.w1 -= self.learning_rate * d_w1
-        self.b1 -= self.learning_rate * d_b1
+    def backward(self, inputs, targets, predictions):
+        error = predictions - targets
+        grad_weights_hidden_output = np.dot(self.hidden_layer_output.T, error) / len(inputs)
+        grad_bias_output = np.mean(error, axis=0)
+        grad_hidden_layer_output = np.dot(error, self.weights_hidden_output.T) * self.sigmoid_derivative(self.hidden_layer_output)
+        grad_weights_input_hidden = np.dot(inputs.T, grad_hidden_layer_output) / len(inputs)
+        grad_bias_hidden = np.mean(grad_hidden_layer_output, axis=0)
 
-    def train(self, x, y, epochs=1000):
-        losses = []
+        self.weights_hidden_output -= self.learning_rate * grad_weights_hidden_output
+        self.bias_output -= self.learning_rate * grad_bias_output
+        self.weights_input_hidden -= self.learning_rate * grad_weights_input_hidden
+        self.bias_hidden -= self.learning_rate * grad_bias_hidden
+
+    def train(self, inputs, targets, epochs=1000):
+        loss_history = []
         for epoch in range(epochs):
-            y_pred = self.forward(x)
-            loss = np.mean((y_pred - y) ** 2)
-            losses.append(loss)
-            self.backward(x, y, y_pred)
-        return losses
+            predictions = self.forward(inputs)
+            loss = np.mean((predictions - targets) ** 2)
+            loss_history.append(loss)
+            self.backward(inputs, targets, predictions)
+        return loss_history
 
-x_train = x.reshape(-1, 1)
-y_train = y_true.reshape(-1, 1)
+x_train = x_values.reshape(-1, 1)
+y_train = y_actual.reshape(-1, 1)
 
-nn = SimpleNeuralNetwork(input_dim=1, hidden_dim=10, output_dim=1, learning_rate=0.01)
-losses = nn.train(x_train, y_train, epochs=5000)
+nn = SimpleNeuralNetwork(input_size=1, hidden_size=40, output_size=1, learning_rate=0.1)
+loss_history = nn.train(x_train, y_train, epochs=10000)
 
-y_pred = nn.forward(x_train)
+y_predictions = nn.forward(x_train)
 
 plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
-plt.plot(losses, label="Training Loss")
+plt.plot(loss_history, label="Training Loss")
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
 plt.title("Loss over Time")
 plt.legend()
 
 plt.subplot(1, 2, 2)
-plt.plot(x, y_true, color="blue", label="True Function", linestyle='dashed')
-plt.plot(x, y_pred.flatten(), color="red", label="Neural Network Approximation")
+plt.plot(x_values, y_actual, color="blue", label="True Function", linestyle='dashed')
+plt.plot(x_values, y_predictions.flatten(), color="red", label="Neural Network Approximation")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Function Approximation")
--- Part D/example.py
+++ Part D/example.py
@@ -1,105 +1,87 @@
 import numpy as np
 from random import random
 
-#because a lot of data will be needed, we will use a class approach.
-class Full_NN(object):
-#A Multi Layer Neural Network class. We use this as for the way we need variables is better suited.
+class NeuralNetwork(object):
 
-    def __init__(self, X=2, HL=[2,2], Y=2): #a constructor for some default values.
-        self.X=X #inputs
-        self.HL=HL #hidden layers
-        self.Y=Y #outputs
- #we are setting up some class variables for our inputs.
-        L=[X]+HL+[Y] #total number of layers. This creates a representation of the
- #the network in the format we need it. i.e array of the format [how many inputs, how mnay hidden layers. how many outputs]
-        W=[] #initialize a weight array
-        for i in range(len(L)-1): #we want to be able go to the next layer up so we set one minus
-            w=np.random.rand(L[i], L[i+1]) #fill them up with random values, that is why we need the numpy library
-            W.append(w) #add the new values to the array.
-        self.W=W #link the class variable to the current variable.
-        Der=[] #initialize a derivative array. This are needed to calculate the back propagation. they are the derivatives of the activation function.
-        for i in range(len(L)-1): 
-            d=np.zeros((L[i], L[i+1])) 
-            Der.append(d)
-        self.Der=Der
- #we will be passing these here as that way the class variable will keep them fur us until we need them.
+    def __init__(self, num_inputs=2, hidden_layers=[2, 2], num_outputs=2):
+        self.num_inputs = num_inputs
+        self.hidden_layers = hidden_layers
+        self.num_outputs = num_outputs
+        layer_sizes = [num_inputs] + hidden_layers + [num_outputs]
+        self.weights = []
+        for i in range(len(layer_sizes) - 1):
+            weight_matrix = np.random.rand(layer_sizes[i], layer_sizes[i + 1])
+            self.weights.append(weight_matrix)
+        self.weights = self.weights
 
-        out=[] #initialize output array
-        for i in range(len(L)): #We don't need to go +1. The outputs are straignt forward.
-            o=np.zeros(L[i]) #we don't need random values, just to have them ready to be used. we fill up with zeros.
-            out.append(o)
-        self.out=out
+        self.deltas = []
+        for i in range(len(layer_sizes) - 1):
+            delta_matrix = np.zeros((layer_sizes[i], layer_sizes[i + 1]))
+            self.deltas.append(delta_matrix)
+        self.deltas = self.deltas
 
-    def FF(self,x): #This method will run the network forward
-        out=x #the input layer output is just the input
+        self.outputs = []
+        for i in range(len(layer_sizes)):
+            output_array = np.zeros(layer_sizes[i])
+            self.outputs.append(output_array)
+        self.outputs = self.outputs
 
-        self.out[0]=x #begin the linking of outputs to the class variable for back propagation. (begin with the input layer.
-        for i, w in enumerate(self.W): #go through (iterate) the network layers via the weights variable
-            Xnext=np.dot(out, w) #calculate product between weights and output for the next output
-            out=self.sigmoid(Xnext) #use the activation function as we must per theory.
-            self.out[i+1]=out #pass the result to the clas variable to preserve for later (when we do the back propagation.
-        return out #return the outputs of the layers
-        
-    def BP(self, Er): #back propagation method. this works by using the Output Error (Er) to go backwards through the layers and calculate the errors needed to update the Weights.
- #this will return the final error of the input.
-        for i in reversed (range(len(self.Der))): #this is a trick allowed by Python, we can go back in reverse and essentially go backwards into the network.
- #so we are iterating backwards through the layers.
- #based on the back propagation equations
- # dE/DWi =(y - y[i+1]) S'(x[i+1])xi
- #S' (x[i+1])=S(x[i+1])(1-s(x[i+1)))
- #s(x[i+1]=x[i+1]
- #x[i+1]=yiWi
-            out=self.out[i+1] #we get the layer output for the previous layer (we are going in reverse)
+    def forward_pass(self, inputs):
+        current_output = inputs
+        self.outputs[0] = inputs
+        for i, weight_matrix in enumerate(self.weights):
+            next_output = np.dot(current_output, weight_matrix)
+            current_output = self.sigmoid(next_output)
+            self.outputs[i + 1] = current_output
+        return current_output
 
-            D=Er*self.sigmoid_Der(out) #we are applying the derivative of the activation function to get Delta.Delta is (y - y[i+1]) S'(x[i+1])
-            D_fixed=D.reshape(D.shape[0], -1).T #Python trick to turn Delta into an array of appropriate size
-            this_out=self.out[i] #current layer output.
+    def backpropagation(self, error):
+        for i in reversed(range(len(self.deltas))):
+            layer_output = self.outputs[i + 1]
+            delta = error * self.sigmoid_derivative(layer_output)
+            delta = delta.reshape(delta.shape[0], -1).T
+            previous_layer_output = self.outputs[i]
+            previous_layer_output = previous_layer_output.reshape(previous_layer_output.shape[0], -1)
+            self.deltas[i] = np.dot(previous_layer_output, delta)
+            error = np.dot(delta, self.weights[i].T)
 
-            this_out=this_out.reshape(this_out.shape[0],-1) #reshape as before to get column array suitable for the multiplication we need.
-            self.Der[i]=np.dot(this_out, D_fixed) #do the matrix multiplication and pass result to class variable.
-            Er=np.dot(D, self.W[i].T) #this is the trick, as this essentially back propagates the next error we need for the next iteration. This error term
-            # is part of the dE/DWi equation for the next layer down in the back propagation, and we pass it on after calculating it in this iteration.
-    def train_nn(self, x, target, epochs, lr): #training the network. The x is an array, the target is an array the epochs is a number and the lr is a number.
-        for i in range (epochs): #training loop for as many epochs as we need
-            S_errors=0 #variable to carry the error we need to report to the user
-            for j, input in enumerate (x): #iterate through the traning data
-                t=target[j]
-                output=self.FF(input) #use the network calculations for forward calculations.
-                e=t-output #obtain the overall Network output error
-                self.BP(e) # use that error to do the back propagation
-                self.GD(lr) #Do gradient descent
-                S_errors+=self.msqe(t,output) #update the overall error to show the user.
-    def GD(self, lr=0.05): #Gradient descent
-        for i in range(len(self.W)): #go through the weights
-            W=self.W[i]
-            Der=self.Der[i]
-            W+= Der*lr #update the weights by applying the learning rate
-    def sigmoid(self,x): #Sigmoid activation function
-        y=1.0/(1+np.exp(-x))
-        return y
-    def sigmoid_Der(self, x): #sigmoid function nderivative
-        sig_der=x*(1.0-x)
-        return sig_der
-    def msqe(self, t, output): #mean square error
-        msq=np.average((t-output)**2)
-        return msq
+    def train(self, inputs, targets, epochs, learning_rate):
+        for epoch in range(epochs):
+            total_error = 0
+            for i, input_data in enumerate(inputs):
+                target = targets[i]
+                output = self.forward_pass(input_data)
+                error = target - output
+                self.backpropagation(error)
+                self.gradient_descent(learning_rate)
+                total_error += self.mean_squared_error(target, output)
 
-if __name__ == "__main__": #Test what we have done
-    training_inputs = np.array([[random()/2 for _ in range(2)] for _ in range(1000)]) #this creates a training set of inputs
+    def gradient_descent(self, learning_rate=0.05):
+        for i in range(len(self.weights)):
+            self.weights[i] += self.deltas[i] * learning_rate
+
+    def sigmoid(self, x):
+        return 1.0 / (1 + np.exp(-x))
+
+    def sigmoid_derivative(self, x):
+        return x * (1.0 - x)
+
+    def mean_squared_error(self, target, output):
+        return np.average((target - output) ** 2)
+
+if __name__ == "__main__":
+    training_inputs = np.array([[random() / 2 for _ in range(2)] for _ in range(1000)])
     targets = np.array([[i[0] * i[1]] for i in training_inputs])
-#this creates a training set of outputs
 
-    nn=Full_NN(2, [5, 5], 1) #creates a NN with 2 inputs, 2 hidden
-    nn.train_nn(training_inputs, targets, 10, 0.1) #trains the network
-    #Testing data to identify if Network trained well.
-    input = np.array([0.3, 0.2]) #after training this tests the
-    target = np.array([0.06]) # for this target value.
-    NN_output = nn.FF(input)
+    nn = NeuralNetwork(2, [5, 5], 1)
+    nn.train(training_inputs, targets, 10, 0.1)
 
-    print("=============== Testing the Network Screen Output ===============")
-    print ("Test input is ", input)
-    print()
-    print("Target output is ",target)
-    print()
-    print("Neural Network actual output is ",NN_output, "there is an error (not MSQE) of ",target-NN_output)
-    print("=================================================================") 
+    test_input = np.array([0.3, 0.2])
+    expected_output = np.array([0.06])
+    nn_output = nn.forward_pass(test_input)
+
+    print("=============== Testing the Network Output ===============")
+    print("Test input: ", test_input)
+    print("Expected output: ", expected_output)
+    print("Neural Network actual output: ", nn_output, "Error: ", expected_output - nn_output)
+    print("=========================================================")
